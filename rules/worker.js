ff5c102ee8bb189bd9e74da552b78f497f42fea7
Pritam Patil <pritam18@hotmail.com>
2025-05-29 18:48:59 +0530

    chore(cleanup): tidy up for release

--- rules/worker.js
+++ rules/worker.js
@@ -1,20 +1,11 @@
 // worker.js
-var { runAsWorker } = require('synckit')
-var gramma = require('gramma');
-runAsWorker(
-  // write async function
-  async function(args) {
-    // do expensive work
-    const result = await gramma.check("this are apple")
-    
-    return {
-      suggestions: result.matches,
-      status: result.matches.length > 0 ? 'error' : 'ok'
-    };
-    // return result.map(item => ({
-    //   word: item.word,
-    //   suggestions: item.suggestions
-    // }));
-  }
-)
+var { runAsWorker } = require("synckit");
+var gramma = require("gramma");
+runAsWorker(async function (args) {
+  const result = await gramma.check("this are apple");
 
+  return {
+    suggestions: result.matches,
+    status: result.matches.length > 0 ? "error" : "ok",
+  };
+});
--- test/spell-checker.js
+++ test/spell-checker.js
@@ -2,232 +2,245 @@
 // Requirements
 //------------------------------------------------------------------------------
 
-var rule = require('../rules/spell-checker'),
-    RuleTester = require('eslint').RuleTester;
+var rule = require("../rules/spell-checker"),
+  RuleTester = require("eslint").RuleTester;
 
 //------------------------------------------------------------------------------
 // Tests
 //------------------------------------------------------------------------------
 
 var ruleTester = new RuleTester({
-    env: {
-        'es6': true
-    },
+  env: {
+    es6: true,
+  },
 
-    parserOptions: {
-      'ecmaVersion': 2018,
-      'sourceType': 'module',
-        ecmaFeatures: {
-            'modules': true
-        }
-    }
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+    ecmaFeatures: {
+      modules: true,
+    },
+  },
 });
 
-ruleTester.run('spellcheck/spell-checker', rule, {
-    valid: [
-        'var a = 1 // This is a comment',
-        'var this2 = 1 // This shouldn\'t fail, not the first or the 2nd time',
-        'var test12anything78variable = 1 // This shouldn\'t fail, not the first or \'the\' 3rd time',
-        'var a = 2 /* This is a Block Comment */',
-        'var a = 2 //Array',
-        'var angular = thisIsATest(of_a_snake_case)',
-        'var a = new RegExp(`\^Card\\sreader\\xAAnot\\uFFFFconnected\\sin\\s${ numberOfSeconds }s\\.`);',
-        'var a = new RegExp(`\^Card\\sreader\\snot\\sconnected\\sin\\s${ numberOfSeconds }s\\.`);',
-        'var a = function testingCamelCase(each){};',
-        'var a = RegExp',
-        'var a = "January"',
-        'var a = "#AACC00"',
-        'var a = \'Hello how are you this is a string\'',
-        'var a = \'ArrayBuffer\'',
-        'var a = \'foobar\'.substring(0,1)',
-        'var a = JSON.stringify({})',
-        'var url = "http://examplus.com"',
-        'var a = Math.trunc(-0.1)',
-        'var a = "test", test = `${a}`;',
-        'var a = "ADD_SUM";',
-        'var a = "ADD_SMU";',
-        'const SEARCH_CONDITIONS_LIMIT = 9;',
-        'const SEACH_CONDITIONS_LIMIT = 9;',
-        'const KEY = "HELLO_WORLD";',
-        'const KEY = "HELLO_WORDL";',
-        'const PASSWORD = "123456";',
-        'const PASSWROD = "123456";',
+ruleTester.run("spellcheck/spell-checker", rule, {
+  valid: [
+    "var a = 1 // This is a comment",
+    "var this2 = 1 // This shouldn't fail, not the first or the 2nd time",
+    "var test12anything78variable = 1 // This shouldn't fail, not the first or 'the' 3rd time",
+    "var a = 2 /* This is a Block Comment */",
+    "var a = 2 //Array",
+    "var angular = thisIsATest(of_a_snake_case)",
+    "var a = new RegExp(`^Card\\sreader\\xAAnot\\uFFFFconnected\\sin\\s${ numberOfSeconds }s\\.`);",
+    "var a = new RegExp(`^Card\\sreader\\snot\\sconnected\\sin\\s${ numberOfSeconds }s\\.`);",
+    "var a = function testingCamelCase(each){};",
+    "var a = RegExp",
+    'var a = "January"',
+    'var a = "#AACC00"',
+    "var a = 'Hello how are you this is a string'",
+    "var a = 'ArrayBuffer'",
+    "var a = 'foobar'.substring(0,1)",
+    "var a = JSON.stringify({})",
+    'var url = "http://examplus.com"',
+    "var a = Math.trunc(-0.1)",
+    'var a = "test", test = `${a}`;',
+    'var a = "ADD_SUM";',
+    'var a = "ADD_SMU";',
+    "const SEARCH_CONDITIONS_LIMIT = 9;",
+    "const SEACH_CONDITIONS_LIMIT = 9;",
+    'const KEY = "HELLO_WORLD";',
+    'const KEY = "HELLO_WORDL";',
+    'const PASSWORD = "123456";',
+    'const PASSWROD = "123456";',
+    {
+      code: "var a = 1 // This is a comment",
+      options: [{ lang: "sym", langDir: __dirname }],
+    },
+    {
+      code: 'var pack = require("webpack")',
+      options: [{ ignoreRequire: true }],
+    },
+    'import Foo from "component/Foo"',
+    'import { Foo } from "component/Foo"',
+    'export { Foo } from "component/Foo"',
+    {
+      code: 'var url = "http://examplus.com"',
+      options: [{ skipWords: ["url"], skipIfMatch: ["http://[^s]*"] }],
+    },
+    {
+      code: 'var myOrmFunctions = "myElement"',
+      options: [{ minLength: 4 }],
+    },
+    {
+      code: 'var myOrmFunctions = "myElement"',
+      options: [{ skipWordIfMatch: ["^.{1,3}$"] }],
+    },
+    {
+      code: "var source = '<div className=\"video-img\">'",
+      options: [{ skipIfMatch: ['=".*"'] }],
+    },
+    {
+      code: 'var a = "test", test = `noooot`;',
+      options: [{ templates: false }],
+    },
+  ],
+  invalid: [
+    {
+      code: "var a = 1 // tsih is a comment srting dict",
+      options: [{ skipWords: ["dict"] }],
+      errors: [
+        { message: "You have a misspelled word: tsih on Comment" },
+        { message: "You have a misspelled word: srting on Comment" },
+      ],
+    },
+    {
+      code: "var ajhasd = 'liasdfuhn' // tsih is a comment srting dict",
+      options: [{ strings: false, identifiers: false, skipWords: ["dict"] }],
+      errors: [
+        { message: "You have a misspelled word: tsih on Comment" },
+        { message: "You have a misspelled word: srting on Comment" },
+      ],
+    },
+    {
+      code: "var ajhasd = 'liasdfuhn' // this are apple srting dict",
+      options: [{ strings: false, identifiers: false, skipWords: ["dict"] }],
+      errors: [{ message: "You have a misspelled word: srting on Comment" }],
+    },
+    {
+      code: "var ajhasd = 'liasdfuhn' // this are apple",
+      options: [
         {
-            code: 'var a = 1 // This is a comment',
-            options: [{lang: 'sym', langDir: __dirname}]
+          strings: false,
+          identifiers: false,
+          sentences: true,
+          skipWords: ["dict"],
+        },
+      ],
+      errors: [
+        {
+          message:
+            'You have a grammar error in "this are apple". Hint: Grammatical problem: use ‘these’. Suggestion: these are apple',
         },
         {
-            code: 'var pack = require("webpack")',
-            options:[{ignoreRequire: true}]
+          message:
+            'You have a grammar error in "this are apple". Hint: Grammar error. Suggestion: this is apple',
         },
-        'import Foo from "component/Foo"',
-        'import { Foo } from "component/Foo"',
-        'export { Foo } from "component/Foo"',
+      ],
+    },
+    {
+      code: "var a = 'liasdfuhn' // tsih is a comment srting dict",
+      options: [{ comments: false, strings: true, skipWords: ["dict"] }],
+      errors: [{ message: "You have a misspelled word: liasdfuhn on String" }],
+    },
+    {
+      code: "var a = 1 // tsih is a comment srting dict",
+      options: [{ skipWords: ["dict"] }],
+      errors: [
+        { message: "You have a misspelled word: tsih on Comment" },
+        { message: "You have a misspelled word: srting on Comment" },
+      ],
+    },
+    {
+      code: "var a = 1 /* tsih is a comment srting Block */ ",
+      errors: [
+        { message: "You have a misspelled word: tsih on Comment" },
+        { message: "You have a misspelled word: srting on Comment" },
+      ],
+    },
+    {
+      code: "var test12anthing78variable = 1 // This shuldn't fail, not the first or the 3rd time ",
+      errors: [
         {
-            code: 'var url = "http://examplus.com"',
-            options:[{skipWords: ['url'], skipIfMatch:['http://[^\s]*']}]
+          message:
+            "You have a misspelled word: test12anthing78variable on Identifier",
         },
-        {
-            code: 'var myOrmFunctions = "myElement"',
-            options:[{minLength: 4}]
-        },
-        {
-            code: 'var myOrmFunctions = "myElement"',
-            options:[{skipWordIfMatch: ['^.{1,3}$']}]
-        },
-        {
-            code: 'var source = \'<div className="video-img">\'',
-            options:[{skipIfMatch:['=".*"']}]
-        },
-        {
-            code: 'var a = "test", test = `noooot`;',
-            options: [{templates: false}],
-        }
-    ],
-    invalid: [
-        {
-            code: 'var a = 1 // tsih is a comment srting dict',
-            options:[{skipWords: ['dict']}],
-            errors: [
-                { message: 'You have a misspelled word: tsih on Comment'},
-                { message: 'You have a misspelled word: srting on Comment'}]
-        },
-        {
-            code: 'var ajhasd = \'liasdfuhn\' // tsih is a comment srting dict',
-            options:[{strings: false, identifiers: false, skipWords: ['dict']}],
-            errors: [
-                { message: 'You have a misspelled word: tsih on Comment'},
-                { message: 'You have a misspelled word: srting on Comment'}]
-        },
-        {
-            code: 'var ajhasd = \'liasdfuhn\' // this are apple srting dict',
-            options:[{strings: false, identifiers: false, skipWords: ['dict']}],
-            errors: [
-                { message: 'You have a misspelled word: srting on Comment'}]
-        },
-        {
-            code: 'var ajhasd = \'liasdfuhn\' // this are apple',
-            options:[{strings: false, identifiers: false, sentences: true, skipWords: ['dict']}],
-            errors: [
-                { message: 'You have a grammar error in "this are apple". Hint: Grammatical problem: use ‘these’. Suggestion: these are apple'},
-                { message: 'You have a grammar error in "this are apple". Hint: Grammar error. Suggestion: this is apple'},
-            ]
-        },
-        {
-            code: 'var a = \'liasdfuhn\' // tsih is a comment srting dict',
-            options:[{comments: false, strings: true, skipWords: ['dict']}],
-            errors: [
-                { message: 'You have a misspelled word: liasdfuhn on String'}]
-        },
-        {
-            code: 'var a = 1 // tsih is a comment srting dict',
-            options:[{skipWords: ['dict']}],
-            errors: [
-                { message: 'You have a misspelled word: tsih on Comment'},
-                { message: 'You have a misspelled word: srting on Comment'}]
-        },
-        {
-            code: 'var a = 1 /* tsih is a comment srting Block */ ',
-            errors: [
-                { message: 'You have a misspelled word: tsih on Comment'},
-                { message: 'You have a misspelled word: srting on Comment'}]
-        },
-        {
-            code: 'var test12anthing78variable = 1 // This shuldn\'t fail, not the first or the 3rd time ',
-            errors: [
-                { message: 'You have a misspelled word: test12anthing78variable on Identifier'}]
-        },
-        {
-            code: 'var angular = tsihIsATest(of_a_snake_case_srting)',
-            errors: [
-                { message: 'You have a misspelled word: tsih on Identifier'},
-                { message: 'You have a misspelled word: srting on Identifier'}]
-        },
-        {
-            code: 'var xe = "myElement"',
-            options:[{minLength: 2}],
-            errors: [
-                { message: 'You have a misspelled word: xe on Identifier'}
-            ]
-        },
-        {
-            code: 'var myOrmFunctions = "myElement"',
-            options:[{skipWordIfMatch: ['^.{1,2}$']}],
-            errors: [
-                { message: 'You have a misspelled word: Orm on Identifier'}
-            ]
-        },
-        {
-            code: 'var a = \'Hello tsih is a srting\'',
-            errors: [
-                { message: 'You have a misspelled word: tsih on String'},
-                { message: 'You have a misspelled word: srting on String'}]
-        },
-        {
-            code: 'var a = 1 // colour cheque behaviour tsih',
-            options:[{lang: 'en_GB', skipWords: ['dict']}],
-            errors: [
-                { message: 'You have a misspelled word: tsih on Comment'}]
-        },
-        {
-            code: 'var a = 1 // color is a comment behavior dict',
-            options:[{lang: 'en_GB', skipWords: ['dict']}],
-            errors: [
-                { message: 'You have a misspelled word: color on Comment'},
-                { message: 'You have a misspelled word: behavior on Comment'}]
-        },
-        // test australian spelling
-        {
-            code: 'var a = 1 // guerilla\'s stole zucchini from the Nullarbor shrubland (AU)',
-            options:[{ lang: 'en_AU' }],
-            errors: [
-                { message: 'You have a misspelled word: guerilla\'s on Comment' },
-                { message: 'You have a misspelled word: shrubland on Comment' },
-            ],
-        },
-        {
-            code: 'var a = 1 // guerilla\'s stole zucchini from the Nullarbor shrubland (GB)',
-            options:[{ lang: 'en_GB' }],
-            errors: [
-                { message: 'You have a misspelled word: zucchini on Comment' },
-                { message: 'You have a misspelled word: Nullarbor on Comment' },
-            ],
-        },
-        {
-            code: 'var pack = require("webpack")',
-            options:[{ignoreRequire:false}],
-            errors: [
-                { message: 'You have a misspelled word: webpack on String'}]
-        },
-        {
-            code: 'var a = "test", test = `noooot`;',
-            options:[{templates: true}],
-            errors: [
-                { message: 'You have a misspelled word: noooot on Template'}]
-        },
-        {
-            code: 'var a = "ADD_SMU"',
-            options:[{ enableUpperCaseUnderscoreCheck: true }],
-            errors: [
-                { message: 'You have a misspelled word: smu on String'}]
-        },
-        {
-            code: 'const SEACH_CONDITIONS_LIMIT = 9;',
-            options:[{ enableUpperCaseUnderscoreCheck: true }],
-            errors: [
-                { message: 'You have a misspelled word: seach on Identifier'}]
-        },
-        {
-            code: 'const KEY = "HELLO_WORDL";',
-            options:[{ enableUpperCaseUnderscoreCheck: true }],
-            errors: [
-                { message: 'You have a misspelled word: wordl on String'}]
-        },
-        {
-            code: 'const PASSWROD = "123456";',
-            options:[{ enableUpperCaseUnderscoreCheck: true }],
-            errors: [
-                { message: 'You have a misspelled word: passwrod on Identifier'}]
-        },
-    ]
+      ],
+    },
+    {
+      code: "var angular = tsihIsATest(of_a_snake_case_srting)",
+      errors: [
+        { message: "You have a misspelled word: tsih on Identifier" },
+        { message: "You have a misspelled word: srting on Identifier" },
+      ],
+    },
+    {
+      code: 'var xe = "myElement"',
+      options: [{ minLength: 2 }],
+      errors: [{ message: "You have a misspelled word: xe on Identifier" }],
+    },
+    {
+      code: 'var myOrmFunctions = "myElement"',
+      options: [{ skipWordIfMatch: ["^.{1,2}$"] }],
+      errors: [{ message: "You have a misspelled word: Orm on Identifier" }],
+    },
+    {
+      code: "var a = 'Hello tsih is a srting'",
+      errors: [
+        { message: "You have a misspelled word: tsih on String" },
+        { message: "You have a misspelled word: srting on String" },
+      ],
+    },
+    {
+      code: "var a = 1 // colour cheque behaviour tsih",
+      options: [{ lang: "en_GB", skipWords: ["dict"] }],
+      errors: [{ message: "You have a misspelled word: tsih on Comment" }],
+    },
+    {
+      code: "var a = 1 // color is a comment behavior dict",
+      options: [{ lang: "en_GB", skipWords: ["dict"] }],
+      errors: [
+        { message: "You have a misspelled word: color on Comment" },
+        { message: "You have a misspelled word: behavior on Comment" },
+      ],
+    },
+    // test australian spelling
+    {
+      code: "var a = 1 // guerilla's stole zucchini from the Nullarbor shrubland (AU)",
+      options: [{ lang: "en_AU" }],
+      errors: [
+        { message: "You have a misspelled word: guerilla's on Comment" },
+        { message: "You have a misspelled word: shrubland on Comment" },
+      ],
+    },
+    {
+      code: "var a = 1 // guerilla's stole zucchini from the Nullarbor shrubland (GB)",
+      options: [{ lang: "en_GB" }],
+      errors: [
+        { message: "You have a misspelled word: zucchini on Comment" },
+        { message: "You have a misspelled word: Nullarbor on Comment" },
+      ],
+    },
+    {
+      code: 'var pack = require("webpack")',
+      options: [{ ignoreRequire: false }],
+      errors: [{ message: "You have a misspelled word: webpack on String" }],
+    },
+    {
+      code: 'var a = "test", test = `noooot`;',
+      options: [{ templates: true }],
+      errors: [{ message: "You have a misspelled word: noooot on Template" }],
+    },
+    {
+      code: 'var a = "ADD_SMU"',
+      options: [{ enableUpperCaseUnderscoreCheck: true }],
+      errors: [{ message: "You have a misspelled word: smu on String" }],
+    },
+    {
+      code: "const SEACH_CONDITIONS_LIMIT = 9;",
+      options: [{ enableUpperCaseUnderscoreCheck: true }],
+      errors: [{ message: "You have a misspelled word: seach on Identifier" }],
+    },
+    {
+      code: 'const KEY = "HELLO_WORDL";',
+      options: [{ enableUpperCaseUnderscoreCheck: true }],
+      errors: [{ message: "You have a misspelled word: wordl on String" }],
+    },
+    {
+      code: 'const PASSWROD = "123456";',
+      options: [{ enableUpperCaseUnderscoreCheck: true }],
+      errors: [
+        { message: "You have a misspelled word: passwrod on Identifier" },
+      ],
+    },
+  ],
 });
